f843a8b87e67748b37b0b62f85eed5e2cefedfef
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2022-12-04 10:48:28 +0530

    Create app.component.html and app.component.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
+type TodoItem={
+  checked:boolean,
+  description: string
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'To-Do-List-App';
+  toDoItems: TodoItem[]=[];
+
+  getItems(complete:boolean=false): TodoItem[]{
+
+    return this.toDoItems.filter(item=>item.checked ===complete);
+  }
+
+  addItem(description: string):void{
+    this.toDoItems.push({
+      checked:false,
+      description:description
+    })
+  }
+  removeItem(item:TodoItem):void{
+    this.toDoItems.splice(this.toDoItems.indexOf(item),1);
+  }
 }
